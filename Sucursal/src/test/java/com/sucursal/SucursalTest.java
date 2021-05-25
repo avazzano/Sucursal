b5f5555f0623923ffba8ac38c49efef254759997
ANDRES VAZZANO <andres.vazzano@gmail.com>
2021-05-25 10:15:19 -0300

    Update Test

--- Sucursal/src/test/java/com/sucursal/SucursalTest.java
+++ Sucursal/src/test/java/com/sucursal/SucursalTest.java
@@ -20,36 +20,41 @@ public class SucursalTest {
 	
 	@Test
 	public void testBuscarSucusalCercana() throws RecursoNoEncontradoException {
-		
+				
 		Sucursal suc1=new Sucursal();
-		suc1.setNombre("La Plata");
-		suc1.setLat(-34.9176179);
-		suc1.setLng(-57.9638704);	
+		suc1.setNombre("Junin");
+		suc1.setDireccion("Roque Saens Peña 222");
+		suc1.setLat(-34.5908286);
+		suc1.setLng(-60.9904505);	
 		sucursalService.create(suc1);
 		
 		Sucursal suc2=new Sucursal();
-		suc2.setNombre("Junin");
-		suc2.setLat(-34.5908286);
-		suc2.setLng(-60.9904505);	
+		suc2.setNombre("Necochea");
+		suc2.setDireccion("AV 2 9999");
+		suc2.setLat(-38.5605203);
+		suc2.setLng(-58.8263163);	
 		sucursalService.create(suc2);
-		
+				
 		Sucursal suc3=new Sucursal();
-		suc3.setNombre("Necochea");
-		suc3.setLat(-38.5605203);
-		suc3.setLng(-58.8263163);	
+		suc3.setNombre("La Plata");
+		suc3.setDireccion("Av 7 888");
+		suc3.setLat(-34.9176179);
+		suc3.setLng(-57.9638704);	
 		sucursalService.create(suc3);
 		
 		Sucursal suc4=new Sucursal();
 		suc4.setNombre("Rosario");
+		suc4.setDireccion("Arenales 741");
 		suc4.setLat(-32.9521898);
 		suc4.setLng(-60.7666798);	
 		sucursalService.create(suc4);	
 		
 		
 		//Coordenadas de CABA -34.6022703,-58.3915682
-		Sucursal sucursalcercana = sucursalService.get(-37.6022703, -57.3915682);
+		// Sucrsal cercana a CABA es La Plata
+		Sucursal sucursalcercana = sucursalService.get(-34.6022703, -58.3915682);
 		
-		assertEquals(sucursalcercana.getId(), suc1.getId());
+		assertEquals(sucursalcercana.getId(), suc3.getId());
 		
 	
 	}
